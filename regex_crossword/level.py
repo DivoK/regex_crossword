ea1f966b93324a77780782dd846295b61059bddf
Divo Kaplan <divokaplan@gmail.com>
2019-09-22 01:23:58 +0300

    Remove file logging from the `level` class.

--- regex_crossword/level.py
+++ regex_crossword/level.py
@@ -8,7 +8,7 @@ LevelDataType = typing.Dict[str, typing.Union[str, typing.List[str]]]
 
 class Level:
     def __init__(self, level_data: LevelDataType):
-        self.title = level_data['title']
+        self.title = level_data.get('title')
         self.up_to_down_regexes = [re.compile(regex) for regex in level_data['up_to_down']]
         self.down_to_up_regexes = [re.compile(regex) for regex in level_data['down_to_up']]
         self.left_to_right_regexes = [
@@ -25,52 +25,30 @@ class Level:
         )
 
     def check_matrix(self, mat: Matrix) -> bool:
-        with open('log.log', 'a') as log:
-            log.write('===============\n')
-            matrix_row_strings = [
-                ''.join(mat[i][j] for j in range(mat.columns)) for i in range(mat.rows)
-            ]
-            matrix_colum_strings = [
-                ''.join(mat[j][i] for j in range(mat.rows)) for i in range(mat.columns)
-            ]
-            log.write('utd: ' + str(self.up_to_down_regexes) + '\n')
-            log.write('dtu: ' + str(self.down_to_up_regexes) + '\n')
-            log.write('ltr: ' + str(self.left_to_right_regexes) + '\n')
-            log.write('rtl: ' + str(self.right_to_left_regexes) + '\n')
-            log.write('mrs: ' + str(matrix_row_strings) + '\n')
-            log.write('mcs: ' + str(matrix_colum_strings) + '\n')
+        matrix_row_strings = [
+            ''.join(mat[i][j] for j in range(mat.columns)) for i in range(mat.rows)
+        ]
+        matrix_colum_strings = [
+            ''.join(mat[j][i] for j in range(mat.rows)) for i in range(mat.columns)
+        ]
 
-            for row, utd_regex, dtu_regex in zip(
-                matrix_colum_strings, self.up_to_down_regexes, self.down_to_up_regexes
+        for row, utd_regex, dtu_regex in zip(
+            matrix_colum_strings, self.up_to_down_regexes, self.down_to_up_regexes
+        ):
+            if (utd_regex.pattern and re.fullmatch(utd_regex, row) is None) or (
+                dtu_regex.pattern and re.fullmatch(dtu_regex, row) is None
             ):
-                log.write(
-                    f'running {row} against {utd_regex.pattern} and {dtu_regex.pattern or None}\n'
-                )
-                if (utd_regex.pattern and re.fullmatch(utd_regex, row) is None) or (
-                    dtu_regex.pattern and re.fullmatch(dtu_regex, row) is None
-                ):
-                    log.write(
-                        f'failed: {utd_regex.pattern} and {dtu_regex.pattern or None} :: {row}\n'
-                    )
-                    return False
+                return False
 
-            for row, ltr_regex, rtl_regex in zip(
-                matrix_row_strings, self.left_to_right_regexes, self.right_to_left_regexes
+        for row, ltr_regex, rtl_regex in zip(
+            matrix_row_strings, self.left_to_right_regexes, self.right_to_left_regexes
+        ):
+            if (ltr_regex.pattern and re.fullmatch(ltr_regex, row) is None) or (
+                rtl_regex.pattern and re.fullmatch(rtl_regex, row) is None
             ):
-                log.write(
-                    f'running {row} against {ltr_regex.pattern} and {rtl_regex.pattern or None}\n'
-                )
-                if (ltr_regex.pattern and re.fullmatch(ltr_regex, row) is None) or (
-                    rtl_regex.pattern and re.fullmatch(rtl_regex, row) is None
-                ):
-                    log.write(
-                        f'failed: {ltr_regex.pattern} and {rtl_regex.pattern or None} :: {row}\n'
-                    )
-                    return False
+                return False
 
-            log.write('True\n')
-
-            return True
+        return True
 
     def format_up_to_down_regexes(self) -> str:
         ret_str = 'up_to_down:\n'
